d89b66c54da7403896e37cc0afb25f02a8d134fc
Ambuj <ambuj.s@devtrust.biz>
2024-03-14 11:17:29 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,110 @@
-import "reflect-metadata";
-import { ApolloServer } from "@apollo/server";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import bodyParser from "body-parser";
-import cors from "cors";
-import express, { Request, Response } from "express";
-import { expressMiddleware } from "@apollo/server/express4";
-import http from "http";
-import path from "path";
-import { resolvers } from "./graphql";
-import { typeDefs } from "./graphql/schema/typedefs";
-import yenv from "yenv";
-import fileUpload from "express-fileupload";
-require("./config/connection");
-import route from "./RestApi/restRoute";
+// import "reflect-metadata";
+// import { ApolloServer } from "@apollo/server";
+// import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import express, { Request, Response } from "express";
+// import { expressMiddleware } from "@apollo/server/express4";
+// import http from "http";
+// import path from "path";
+// import { resolvers } from "./graphql";
+// import { typeDefs } from "./graphql/schema/typedefs";
+// import yenv from "yenv";
+// import fileUpload from "express-fileupload";
+// require("./config/connection");
+// import route from "./RestApi/restRoute";
 
-import { authMiddleware } from "./graphql/schema";
-const startServer = async () => {
-  yenv("env.yaml", { env: "development" });
-  const router = express();
+// import { authMiddleware } from "./graphql/schema";
+// const startServer = async () => {
+//   yenv("env.yaml", { env: "development" });
+//   const router = express();
 
-  router.use(
-    cors({
-      origin: "*",
-      methods: ["GET", "POST"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
+//   router.use(
+//     cors({
+//       origin: "*",
+//       methods: ["GET", "POST"],
+//       allowedHeaders: ["Content-Type", "Authorization"],
+//     })
+//   );
 
-  const publicPath = path.join(__dirname, "public");
-  router.use(express.static(publicPath));
-  router.use(express.json());
-  router.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-  router.get("/", (_req: Request, res: Response) => {
-   res.send("Express Typescript on Vercel");
-  });
-  router.use(
-    fileUpload({
-      useTempFiles: true,
-    })
-  );
-  router.use(route);
-  router.use((req, res, next) => {
-    next();
-  });
-  const httpServer = http.createServer(router);
+//   const publicPath = path.join(__dirname, "public");
+//   router.use(express.static(publicPath));
+//   router.use(express.json());
+//   router.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+//   router.get("/", (_req: Request, res: Response) => {
+//    res.send("Express Typescript on Vercel");
+//   });
+//   router.use(
+//     fileUpload({
+//       useTempFiles: true,
+//     })
+//   );
+//   router.use(route);
+//   router.use((req, res, next) => {
+//     next();
+//   });
+//   const httpServer = http.createServer(router);
 
-  const server: any = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+//   const server: any = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+//   });
 
-  await server.start();
+//   await server.start();
 
-  router.use(
-    "/graphql",
-    cors<cors.CorsRequest>(),
-    bodyParser.json({ limit: "50mb" }),
+//   router.use(
+//     "/graphql",
+//     cors<cors.CorsRequest>(),
+//     bodyParser.json({ limit: "50mb" }),
 
-    expressMiddleware(server, {
-      context: async ({ req, res }) => {
-        const { loginId, userId } = await authMiddleware(req);
+//     expressMiddleware(server, {
+//       context: async ({ req, res }) => {
+//         const { loginId, userId } = await authMiddleware(req);
         
-        return {
-          loginId,
-          userId,
-        };
-      },
-    })
-    // expressMiddleware(server, {})
-  );
+//         return {
+//           loginId,
+//           userId,
+//         };
+//       },
+//     })
+//     // expressMiddleware(server, {})
+//   );
 
-  // await new Promise<void>((resolve) =>
-  //   httpServer.listen(
-  //     {
-  //       port: 5001,
-  //     },
-  //     resolve
-  //   )
-  // );
+//   // await new Promise<void>((resolve) =>
+//   //   httpServer.listen(
+//   //     {
+//   //       port: 5001,
+//   //     },
+//   //     resolve
+//   //   )
+//   // );
 
-  router.listen(5000, () => {
-    // tslint:disable-next-line
-    console.log("🚀 Server ready at http://localhost:5000/graphql");
-  });
-};
-startServer();
+//   router.listen(5000, () => {
+//     // tslint:disable-next-line
+//     console.log("🚀 Server ready at http://localhost:5000/graphql");
+//   });
+// };
+// startServer();
+
+import express, { Application, Request, Response } from "express";
+
+
+const app: Application = express();
+
+
+
+app.use(express.json());
+
+// Default
+app.get("/api", (req: Request, res: Response) => {
+  res.status(201).json({ message: "Welcome to Auth ts" });
+});
+
+// User Route
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (): void => console.log(`Server is running on ${PORT}`));
